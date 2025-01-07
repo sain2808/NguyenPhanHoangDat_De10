1c5fa6c16fa3dba826265b9082906825dcde8233
sain2808 <hoangdat753@gmail.com>
2025-01-07 15:20:47 +0700

    fixloi

--- dethi.sh
+++ dethi.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+
 echo "Nhap so nguyen: "
 read n
-echo "So vua nhap la %d",n;
-
-if test $t -gt 0
-then
-echo "duong"
+echo "So vua nhap la $n"
+# Kiem tra so chinh phuong
+sqrt=$(echo "scale=0; sqrt($n)" | bc)
+if [ $(($sqrt * $sqrt)) -eq $n ]; then
+  echo "$n la so chinh phuong"
 else
-echo "am"
-Fi
+  echo "$n khong phai la so chinh phuong"
+fi
+
 
 
 #Cau tren chua dung. Hay tao ra 1 nhanh moi co ten la fixbug de sua lai cho dung 
