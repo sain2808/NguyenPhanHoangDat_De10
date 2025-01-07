b914001ac11c4495769b9da088ea3ccba71a6b7f
sain2808 <hoangdat753@gmail.com>
2025-01-07 14:53:05 +0700

    baiitap

--- dethi.sh
+++ dethi.sh
@@ -2,7 +2,5 @@
 
 echo "Nhap so nguyen: "
 read n
-echo "So vua nhap la %d",n
-#Cau tren chua dung. Hay tao ra 1 nhanh moi co ten la fixbug de sua lai cho dung 
-#va lam them yeu cau ben duoi 
-#Viet tiep de kiem tra xem n co phai la so chinh phuong khong
+echo "So vua nhap la %d",n;
+
